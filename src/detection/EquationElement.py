93811925c06728e40c3cc4d2219d2b336b68ca36
Antoine <antoine.spahr@epfl.ch>
2020-05-05 22:27:05 +0200

    Detector : get_mask + extract element implemented

--- src/detection/EquationElement.py
+++ src/detection/EquationElement.py
@@ -2,17 +2,17 @@ class EquationElement:
     """
     Define an equation element of the video (digit or operator)
     """
-    def __init__(self, bbox, image):
+    def __init__(self, bbox, image, mask):
         # object bbox within video frame
         self.x0, self.y0 = bbox[0], bbox[1]
         self.x1, self.y1 = bbox[2], bbox[3]
 
-        # the character represented by the image : + - * ÷ = or 0 1 2 3 4 5 6 7 8
-        self.value = ''
-
         # the image array of the element : HxWx3
         self.image = image
         self.mask = mask
+
+        # the character represented by the image : + - * ÷ = or 0 1 2 3 4 5 6 7 8
+        self.value = ''
 
     def get_Fourier_descr(self, N=None):
         """
--- src/detection/detector.py
+++ src/detection/detector.py
@@ -1,4 +1,11 @@
+import matplotlib.pyplot as plt
+import skimage
+import skimage.filters
+import skimage.morphology
+import skimage.segmentation
+import numpy as np
 
+from src.detection.EquationElement import EquationElement
 
 class Detector:
     """
@@ -6,9 +13,10 @@ class Detector:
     """
     def __init__(self, frame, digit_model_path, operator_model_path, color_model_path):
         self.frame = frame
-        self.element_list = None
+        self.element_list = []
 
         # load the models
+
 
     def analyse_frame(self):
         """
@@ -25,15 +33,56 @@ class Detector:
 
     def get_mask(self):
         """
+        produce a binary mask of the objects.
+        """
+        # convert to grayscale
+        img = skimage.color.rgb2gray(self.frame)
+        # apply Otsu thresholding method
+        mask = np.where(img < skimage.filters.threshold_otsu(img), True, False)
+        # Apply some morphologycal operations to clean and connect the objects components
+        mask = skimage.morphology.opening(mask, selem=skimage.morphology.disk(1))
+        mask = skimage.morphology.dilation(mask, selem=skimage.morphology.disk(5))
+        # complete background by region growing on the corners
+        mask = skimage.segmentation.flood_fill(mask, (0,0), 0)
+        mask = skimage.segmentation.flood_fill(mask, (0,mask.shape[1]-1), 0)
+        mask = skimage.segmentation.flood_fill(mask, (mask.shape[0]-1,0), 0)
+        mask = skimage.segmentation.flood_fill(mask, (mask.shape[0]-1,mask.shape[1]-1), 0)
+
+        return mask
+
+    def extarct_equation_element(self, mask):
+        """
 
         """
-        raise(NotImplementedError)
+        # labellise mask and generate the properties
+        labels = skimage.measure.label(mask, background=False)
+        props = skimage.measure.regionprops(labels)
 
-    def extarct_equation_element(self):
-        """
+        for prop in props:
+            # get bounding box
+            y0, x0, y1, x1 = prop.bbox
+            # extract image
+            elem_im = self.frame[y0:y1+1, x0:x1+1, :]
+            # grayscale the image and remake the mask without the dilation
+            elem_im_gray = skimage.color.rgb2gray(elem_im)
+            elem_mask = np.where(elem_im_gray < skimage.filters.threshold_otsu(skimage.color.rgb2gray(elem_im_gray)), True, False)
 
-        """
-        raise(NotImplementedError)
+            # reajust bbox and image to new mask
+            coords = np.argwhere(elem_mask)
+            y_min, x_min = coords.min(axis=0)
+            y_max, x_max = coords.max(axis=0)
+
+            y1 = y0 + y_max+1
+            x1 = x0 + x_max+1
+            y0 += y_min-1
+            x0 += x_min-1
+
+            # crop to content
+            elem_im = elem_im[y_min-1:y_max+2, x_min-1:x_max+2, :]
+            elem_mask = elem_mask[y_min-1:y_max+2, x_min-1:x_max+2]
+
+            # mask EquationElements
+            self.element_list.append(EquationElement((x0, y0, x1, y1), elem_im, elem_mask))
 
     def classify_color(self):
         """
